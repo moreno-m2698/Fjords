bd155b2c9a5edf389bba0de68bdcf9cb344489c2
Michael <moreno.m2698@gmail.com>
2023-11-03 22:47:12 -0400

    Added getMapping's to RiotAccountController

--- src/main/java/howlingabysstool/howlingabysstoolapi/controller/RiotAccountController.java
+++ src/main/java/howlingabysstool/howlingabysstoolapi/controller/RiotAccountController.java
@@ -1,7 +1,9 @@
 package howlingabysstool.howlingabysstoolapi.controller;
 
-import howlingabysstool.howlingabysstoolapi.service.MatchService;
+import howlingabysstool.howlingabysstoolapi.domain.RiotAccount;
 import howlingabysstool.howlingabysstoolapi.service.RiotAccountService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +14,13 @@ public class RiotAccountController {
 
     public RiotAccountController(RiotAccountService riotAccountService) {
         this.riotAccountService = riotAccountService;
+    }
+    @GetMapping("/account/by-puuid/{puuid}")
+    public RiotAccount getRiotAccountByPuuid(@PathVariable String puuid) {
+        return riotAccountService.getRiotAccountByPuuid(puuid);
+    }
+    @GetMapping("/account/by-riot-id/{gameName}/{tagLine}")
+    public RiotAccount getRiotAccountByRiotId(@PathVariable String gameName, @PathVariable String tagLine) {
+        return riotAccountService.getRiotAccountByRiotId(gameName, tagLine);
     }
 }
