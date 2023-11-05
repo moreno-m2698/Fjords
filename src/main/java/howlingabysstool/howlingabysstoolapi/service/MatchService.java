57a5f5a92bf1c574b20fe84a611f857f9640b65f
Michael <moreno.m2698@gmail.com>
2023-11-04 20:41:54 -0400

    Added getMatcchIds function to match service

--- src/main/java/howlingabysstool/howlingabysstoolapi/service/MatchService.java
+++ src/main/java/howlingabysstool/howlingabysstoolapi/service/MatchService.java
@@ -2,6 +2,9 @@ package howlingabysstool.howlingabysstoolapi.service;
 
 import howlingabysstool.howlingabysstoolapi.domain.Match;
 
+import java.util.List;
+
 public interface MatchService {
     public Match getMatchById(String matchId);
+    public List<String> getMatchIdsByPuuid(String puuid, int amount);
 }
