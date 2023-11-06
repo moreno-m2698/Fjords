ab3ed611ee7cfcb71655a9425ec52ce982ec03f5
Michael <moreno.m2698@gmail.com>
2023-11-05 20:49:32 -0500

    MatchTimelineService added

---
+++ src/main/java/howlingabysstool/howlingabysstoolapi/service/MatchTimelineService.java
@@ -0,0 +1,7 @@
+package howlingabysstool.howlingabysstoolapi.service;
+
+import howlingabysstool.howlingabysstoolapi.domain.MatchTimeline;
+
+public interface MatchTimelineService {
+    public MatchTimeline getMatchTimeline(String matchId);
+}
---
+++ src/main/java/howlingabysstool/howlingabysstoolapi/service/MatchTimelineServiceImpl.java
@@ -0,0 +1,22 @@
+package howlingabysstool.howlingabysstoolapi.service;
+
+import howlingabysstool.howlingabysstoolapi.domain.MatchTimeline;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MatchTimelineServiceImpl {
+    private final String apiString = "https://americas.api.riotgames.com/lol/match/v5/matches/";
+    private final String apiKey= "api_key=RGAPI-d804884a-b19c-4c05-8998-8dde27fb7e4b";
+    private final RestTemplate restTemplate;
+
+    public MatchTimelineServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+    @Override
+    public MatchTimeline getMatchTimeline(String matchId) {
+        String apiUrl = apiString + matchId + '/timeline?' + apiKey;
+        MatchTimeline matchTimeline = restTemplate.getForObject(apiUrl, MatchTimeline.class);
+        return matchTimeline;
+    }
+}
