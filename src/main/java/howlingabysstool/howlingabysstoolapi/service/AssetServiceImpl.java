65c528b5a99b2436b036ea6bfdce44821799df43
Michael <moreno.m2698@gmail.com>
2024-02-06 18:52:18 -0500

    updated asset dir path

--- src/main/java/howlingabysstool/howlingabysstoolapi/service/AssetServiceImpl.java
+++ src/main/java/howlingabysstool/howlingabysstoolapi/service/AssetServiceImpl.java
@@ -1,42 +1,37 @@
 package howlingabysstool.howlingabysstoolapi.service;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
 import java.util.Optional;
 
 @Service
-public class AssetServiceImpl implements AssetService{
+public class AssetServiceImpl implements AssetService {
 
     //TODO: CTRL+SHIFT+T for tests
 
     private final String PATCH_VERSION = "14.1.1";
-    private final String ASSET_DIR = ".\\src\\main\\resources\\ddragon\\" + PATCH_VERSION + "\\img\\";
+    private final String ASSET_DIR = "ddragon/" + PATCH_VERSION + "/img/";
+
     public AssetServiceImpl() {
     }
+
     @Override
     public Optional<byte[]> getAsset(String subDir, String asset) {
         String dir = ASSET_DIR + subDir;
         String assetPng = asset + ".png";
-        Path imagePath = Paths.get(dir, assetPng);
+
         try {
-            System.out.println("Retrieving asset at: " + imagePath);
-            byte[] imageBytes = Files.readAllBytes(imagePath);
-            Optional<byte[]> result = Optional.of(imageBytes);
-            return result;
-        } catch (Exception error) {
-
-            //I dont think i need to return anything if there is an error
-            System.out.println("Asset not found at: " + imagePath);
-            Optional<byte[]> result =  Optional.empty();
-            return result;
+            ClassPathResource resource = new ClassPathResource(dir + "/" + assetPng);
+            InputStream inputStream = resource.getInputStream();
+            byte[] imageBytes = inputStream.readAllBytes();
+            System.out.println("Retrieving asset at: " + resource.getPath());
+            return Optional.of(imageBytes);
+        } catch (IOException error) {
+            System.out.println("Asset not found at: " + dir + "/" + assetPng);
+            return Optional.empty();
         }
-
     }
-
-
 }
